bbf72eabebbd917eb552e27be822a1847c15241b
Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
2021-12-09 01:51:13 -0600

    Clippy fixes and rustfmt.
    Signed-off-by: Aalekh Patel <aalekh.gwpeck.7998@icloud.com>

--- src/aoc-2021-09/src/main.rs
+++ src/aoc-2021-09/src/main.rs
@@ -1,38 +1,36 @@
 use std::io;
 use std::io::Read;
 
-
 fn main() {
     let mut buffer: String = String::new();
     io::stdin().read_to_string(&mut buffer).unwrap();
     let grid = parser::read_grid(&buffer);
+    let wall: usize = 9;
 
     let result_part_1 = part_1::solve(&grid);
-    let result_part_2 = part_2::solve(&grid);
+    let result_part_2 = part_2::solve(&grid, wall);
 
     println!("Part 1: {:?}\nPart 2: {:?}", result_part_1, result_part_2);
-
 }
-
 
 mod parser {
     fn read_row(inp: &str) -> Vec<usize> {
-        inp
-            .trim()
+        inp.trim()
             .chars()
-            .map(|x| String::from(x).parse::<usize>().unwrap_or_else(|_| panic!("Couldn't parse {:?}", x)))
+            .map(|x| {
+                String::from(x)
+                    .parse::<usize>()
+                    .unwrap_or_else(|_| panic!("Couldn't parse {:?}", x))
+            })
             .collect::<Vec<usize>>()
     }
 
     pub fn read_grid(inp: &str) -> Vec<Vec<usize>> {
-        inp
-            .split_whitespace()
+        inp.split_whitespace()
             .map(|x| read_row(x))
             .collect::<Vec<Vec<usize>>>()
     }
-
 }
-
 
 pub fn neighbors(row: usize, col: usize, rows: usize, cols: usize) -> Vec<(usize, usize)> {
     if row == 0 {
@@ -47,27 +45,28 @@ pub fn neighbors(row: usize, col: usize, rows: usize, cols: usize) -> Vec<(usize
         if col == 0 {
             vec![(row, col + 1), (row + 1, col), (row - 1, col)]
         } else if col < cols - 1 {
-            vec![(row, col + 1), (row + 1, col), (row, col - 1), (row - 1, col)]
+            vec![
+                (row, col + 1),
+                (row + 1, col),
+                (row, col - 1),
+                (row - 1, col),
+            ]
         } else {
             vec![(row - 1, col), (row + 1, col), (row, col - 1)]
         }
+    } else if col == 0 {
+        vec![(row, col + 1), (row - 1, col)]
+    } else if col < cols - 1 {
+        vec![(row, col + 1), (row, col - 1), (row - 1, col)]
     } else {
-        if col == 0 {
-            vec![(row, col + 1), (row - 1, col)]
-        } else if col < cols - 1 {
-            vec![(row, col + 1), (row, col - 1), (row - 1, col)]
-        } else {
-            vec![(row, col - 1), (row - 1, col)]
-        }
+        vec![(row, col - 1), (row - 1, col)]
     }
 }
-
 
 mod part_1 {
     use super::neighbors;
 
     fn is_sink(grid: &[Vec<usize>], neighbors: &[(usize, usize)], row: usize, col: usize) -> bool {
-
         let largest_neighbor: &(usize, usize) = neighbors
             .iter()
             .min_by_key(|(x, y)| Some(grid[*x][*y]))
@@ -77,7 +76,6 @@ mod part_1 {
     }
 
     pub fn solve(grid: &[Vec<usize>]) -> usize {
-
         let rows: usize = grid.len();
         let cols: usize = grid[0].len();
 
@@ -86,7 +84,7 @@ mod part_1 {
         for row in 0..rows {
             for col in 0..cols {
                 if is_sink(grid, &neighbors(row, col, rows, cols), row, col) {
-                    low_points_score += (grid[row][col] + 1);
+                    low_points_score += grid[row][col] + 1;
                 }
             }
         }
@@ -94,18 +92,15 @@ mod part_1 {
     }
 }
 
-
 mod part_2 {
     use std::collections::BTreeSet;
-    use std::error::Error;
     use std::hash::Hash;
-    use super::neighbors;
 
     // It's weird that BTreeSet doesn't have a method like pop_random()
     // so let's define a custom one.
     fn pop<T>(set: &mut BTreeSet<T>) -> T
     where
-        T: Clone + Eq + Hash + Ord
+        T: Clone + Eq + Hash + Ord,
     {
         let mut it = set.iter();
         let first = it.next().cloned().unwrap();
@@ -114,18 +109,17 @@ mod part_2 {
         first
     }
 
-
     fn flood_fill(grid: &[Vec<usize>], wall: usize) -> Vec<BTreeSet<(usize, usize)>> {
         let rows: usize = grid.len();
         let cols: usize = grid[0].len();
 
         let mut unseen: BTreeSet<(usize, usize)> = BTreeSet::new();
-        let mut seen: BTreeSet<(usize, usize)> = BTreeSet::new();
 
-        for row in 0..rows {
-            for col in 0..cols {
-                if grid[row][col] != wall {
-                    unseen.insert((row, col));
+        #[allow(clippy::needless_range_loop)]
+        for row_idx in 0..rows {
+            for col_idx in 0..cols {
+                if grid[row_idx][col_idx] != wall {
+                    unseen.insert((row_idx, col_idx));
                 }
             }
         }
@@ -141,39 +135,36 @@ mod part_2 {
                 region.insert((current_x, current_y));
 
                 for (shift_x, shift_y) in [(0isize, 1isize), (0, -1), (1, 0), (-1, 0)] {
-                    let (mut neighbor_x, mut neighbor_y): (isize, isize) = (current_x as isize + shift_x, current_y as isize + shift_y);
+                    let (neighbor_x, neighbor_y): (isize, isize) =
+                        (current_x as isize + shift_x, current_y as isize + shift_y);
 
-                    if 0 <= neighbor_x.min(neighbor_y) && neighbor_x < rows as isize && neighbor_y < cols as isize {
-                        if (
-                            !region.contains(&(neighbor_x as usize, neighbor_y as usize)) &&
-                            !seen.contains(&(neighbor_x as usize, neighbor_y as usize)) &&
-                            grid[neighbor_x as usize][neighbor_y as usize] != wall
-                        ) {
-                            queue.push((neighbor_x as usize, neighbor_y as usize));
-                        }
+                    if 0 <= neighbor_x.min(neighbor_y)
+                        && neighbor_x < rows as isize
+                        && neighbor_y < cols as isize
+                        && !region.contains(&(neighbor_x as usize, neighbor_y as usize))
+                        && unseen.contains(&(neighbor_x as usize, neighbor_y as usize))
+                        && grid[neighbor_x as usize][neighbor_y as usize] != wall
+                    {
+                        queue.push((neighbor_x as usize, neighbor_y as usize));
                     }
                 }
             }
-
             for r in &region {
                 unseen.remove(r);
             }
             regions.push(region.clone());
-
         }
 
         regions
     }
 
-    pub fn solve(grid: &[Vec<usize>]) -> usize {
-        let wall: usize = 9;
+    pub fn solve(grid: &[Vec<usize>], wall: usize) -> usize {
         let regions = flood_fill(grid, wall);
 
         let mut region_sizes: Vec<usize> = regions
             .iter()
             .map(|region| region.len())
             .collect::<Vec<usize>>();
-
 
         region_sizes.sort_unstable();
 
@@ -185,7 +176,6 @@ mod part_2 {
         acc
     }
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -209,18 +199,20 @@ mod tests {
         assert_eq!(rows, 5);
         assert_eq!(cols, 10);
 
-        assert_eq!(grid, vec![
-            vec![2, 1, 9, 9, 9, 4, 3, 2, 1, 0],
-            vec![3, 9, 8, 7, 8, 9, 4, 9, 2, 1],
-            vec![9, 8, 5, 6, 7, 8, 9, 8, 9, 2],
-            vec![8, 7, 6, 7, 8, 9, 6, 7, 8, 9],
-            vec![9, 8, 9, 9, 9, 6, 5, 6, 7, 8],
-        ]);
+        assert_eq!(
+            grid,
+            vec![
+                vec![2, 1, 9, 9, 9, 4, 3, 2, 1, 0],
+                vec![3, 9, 8, 7, 8, 9, 4, 9, 2, 1],
+                vec![9, 8, 5, 6, 7, 8, 9, 8, 9, 2],
+                vec![8, 7, 6, 7, 8, 9, 6, 7, 8, 9],
+                vec![9, 8, 9, 9, 9, 6, 5, 6, 7, 8],
+            ]
+        );
     }
 
     #[test]
     fn test_sample_1() {
-
         let inp = "
         2199943210
         3987894921
@@ -238,7 +230,6 @@ mod tests {
 
     #[test]
     fn test_sample_2() {
-
         let inp = "
         2199943210
         3987894921
